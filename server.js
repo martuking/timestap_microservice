0fb22d552e6cfd4e05f10b904d1388dd32c36bfb
System <none>
2017-08-08 17:05:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bo)
+app.use(bodyParser.json());
+app.use(cors());
 // http://expressjs.com/en/starter/static-files.html
 //app.use(express.static('public'));
 
